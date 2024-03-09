d3f8bb5834a83847d45f7b66e5dd959eae8622ec
Mark-Makarov <mark.makararov@yandex.ru>
2024-03-09 20:10:29 +0300

    added configuration for elements, added shadcn ui components

---
+++ components/comments/Comments.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+// libraries
+import { ClientSideSuspense } from "@liveblocks/react";
+
+// components
+import { CommentsOverlay } from "@/components/comments/CommentsOverlay";
+
+export const Comments = () => (
+  <ClientSideSuspense fallback={null}>
+    {() => <CommentsOverlay />}
+  </ClientSideSuspense>
+);
