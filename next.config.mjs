5d577b1fa7b54ed75ee36efe6ae5e6b8f9f21df9
Mark-Makarov <mark.makararov@yandex.ru>
2024-03-13 01:08:13 +0300

    edit docker configs

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,9 @@ const nextConfig = {
           port: "",
       }],
     },
+    env: {
+        LIVEBLOCKS_API_KEY: "pk_dev_uR4fxGH5SYchuZQb0S8wwQXdGxKgsdD5XTYGIAVjZ3W_j_2-9t0uwLc_ukYOzchW"
+    }
 };
 
 export default nextConfig;
